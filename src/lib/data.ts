99a5c6d87dd80c2a08c6ae71c7e907d0b73d51f3
luciano <luciano.delapena@gmail.com>
2026-09-15 18:53:15 -0300

    fix landing

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -83,7 +83,7 @@ export const copy = {
         tags: ["React", "TypeScript", "TailwindCSS"],
         image: "/projects/landing.png",
         repo: "",
-        live: "https://portfolio-landing-three-psi.vercel.app/",
+        live: "https://lucianodelapena.com/landing",
       },
 
       {
@@ -232,7 +232,7 @@ export const copy = {
         tags: ["React", "TypeScript", "TailwindCSS"],
         image: "/projects/landing.png",
         repo: "",
-        live: "https://portfolio-landing-three-psi.vercel.app/",
+        live: "https://lucianodelapena.com/landing",
       },
       {
         title: "Evently",
